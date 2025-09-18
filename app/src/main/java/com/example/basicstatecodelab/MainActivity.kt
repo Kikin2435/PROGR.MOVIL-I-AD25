989890269f1167deb929baa6a1debcc16b2168de
Kikin <fj.kikin@gmail.com>
2025-09-18 13:51:24 -0600

    Se agrego funcionamiento al boton para que incremento en 1 al ser presionado

--- app/src/main/java/com/example/basicstatecodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicstatecodelab/MainActivity.kt
@@ -17,6 +17,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.basicstatecodelab.ui.theme.BasicStateCodelabTheme
 import androidx.compose.material3.Button
 import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,7 +51,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
+        var count by remember { mutableStateOf(0) }
+
         Text("You've had $count glasses.")
         Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
